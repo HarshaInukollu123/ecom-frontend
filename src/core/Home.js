bc18eef0ae93aa0a2f0f56930b9e3aae2849be83
HarshaInukollu123 <harshainukollu3@gmail.com>
2021-04-17 12:16:42 +0530

    initial commit

---
+++ src/core/Home.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+import {getproducts} from './helper/coreapicalls';
+import Base from './Base';
+import Card from './Card';
+import "../styles.css"
+
+export default function Home(){
+  const [products, setProducts] = useState([])
+  const [error, setError]= useState(false)
+
+  const loadAllProducts = () => {
+    getproducts().then(data=>{
+      if(data.error)
+      {
+        setError(data.error)
+        console.log(error);
+      }
+      else
+      {
+        setProducts(data)
+      }
+    })
+  }
+
+useEffect(()=>{
+  loadAllProducts();
+}, [])
+
+  return(
+    <Base title="HomePage" description="Welcome to Tshirt Store">
+    <div>
+      <h1>Home Component</h1>
+        <div className='row'>
+          {products.map((product, index) => {
+            return(
+              <div key={index} className="col-4 mb-4">
+                <Card product={product}/>
+              </div>
+            )
+          })}
+        </div>
+    </div>
+    </Base>
+  );
+}
