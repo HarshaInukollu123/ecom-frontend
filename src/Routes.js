4f089dbef183a006cdc7fde025d135f4604e1232
HarshaInukollu123 <harshainukollu3@gmail.com>
2021-04-22 11:42:52 +0530

    Added signup component

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
+import UserDashboard from "./user/UserDashboard";
+import PrivateRoutes from "./auth/helper/PrivateRoute";
 import Home from "./core/Home";
+import Signup from "./user/Signup";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
+        <PrivateRoutes path="/user/dashboard" exact component={UserDashboard} />
+
       </Switch>
     </BrowserRouter>
   );
---
+++ src/user/UserDashboard.js
@@ -0,0 +1,10 @@
+import Base from '../core/Base';
+
+const UserDashboard = () => {
+  return(
+    <Base title="User Dashboard">
+      <h1>welcome to user dashboard.</h1>
+    </Base>
+  );
+}
+export default UserDashboard
